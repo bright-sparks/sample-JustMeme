ff36df2f316162bd1b7c6cd4f4dd4b6962880444
TJ VanToll <tj.vantoll@gmail.com>
2015-05-18 14:18:19 -0400

    Also watch for XML and CSS changes

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,10 @@ var babel = require("gulp-babel");
 var jscs = require("gulp-jscs");
 var jshint = require("gulp-jshint");
 var rm = require("gulp-rm");
+var runSequence = require("run-sequence");
 var watch = require("gulp-watch");
 
+var resources = ["src/*.{css,js,xml}", "src/**/*.{css,js,md,xml}"];
 var jsFiles = ["src/shared/**/*.js", "src/views/**/*.js"];
 
 gulp.task("jscs", function() {
@@ -20,7 +22,7 @@ gulp.task("jshint", function() {
 
 gulp.task("lint", ["jshint", "jscs"]);
 
-gulp.task("babel", function() {
+gulp.task("compile", function() {
 	return gulp.src(jsFiles, { base: "src" })
 		.pipe(babel())
 		.pipe(gulp.dest("app"));
@@ -31,13 +33,20 @@ gulp.task("clean", function() {
 		.pipe(rm());
 });
 
-gulp.task("copy", ["clean"], function() {
+gulp.task("copyAll", ["clean"], function() {
 	return gulp.src(["**/*"], { base: "src" })
 		.pipe(gulp.dest("app"));
 });
 
-gulp.task("watch", ["copy"], function() {
-	return gulp.watch(jsFiles, ["babel"]);
+gulp.task("copyResources", function() {
+	return gulp.src(resources, { base: "src" })
+		.pipe(gulp.dest("app"));
+});
+
+gulp.task("watch", ["copyAll"], function() {
+	return gulp.watch(resources, function() {
+		runSequence("copyResources", "compile");
+	});
 });
 
 gulp.task( "default", ["watch"]);
